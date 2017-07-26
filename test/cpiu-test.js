f856ef12b8c9c9eaaa2d9c7d56865b77ebdf99d3
Cody Logan <clpo13@gmail.com>
2017-07-26 15:40:17 -0700

    Remove API tests

--- test/cpiu-test.js
+++ test/cpiu-test.js
@@ -1,20 +1,24 @@
 const assert = require('assert')
-cpiu = require('../index')
+const cpiu = require('../index')
 
 describe('cpiu', function () {
   describe('singleSeries', function () {
+    /*
     it('should return the right status', function () {
       cpiu.singleSeries().then(function (res) {
         assert(res.status === 'REQUEST_SUCCEEDED')
       })
     })
+    */
   })
 
   describe('singleSeriesWithOptions', function () {
+    /*
     it('should return the right status', function () {
       cpiu.singleSeriesWithOptions(1999, 2000).then(function (res) {
         assert(res.status === 'REQUEST_SUCCEEDED')
       })
     })
+    */
   })
 })
