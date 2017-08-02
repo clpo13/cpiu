8b892199a43efabd32a7b1524525552011cbd609
Cody Logan <cody@madrants.net>
2017-08-02 11:41:11 -0700

    Mock HTTP requests with Nock

--- test/cpiu-test.js
+++ test/cpiu-test.js
@@ -14,27 +14,254 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-// const assert = require('assert')
-// const cpiu = require('../index')
+const expect = require('chai').expect
+const nock = require('nock')
+const cpiu = require('../index')
 
 describe('cpiu', function () {
   describe('singleSeries', function () {
-    /*
+    beforeEach(function () {
+      var singleSeriesResponse = {
+        'status': 'REQUEST_SUCCEEDED',
+        'responseTime': 30,
+        'message': [],
+        'Results': {
+          'series': [{
+            'seriesID': 'CUUR0000SA0',
+            'data': [{
+              'year': '2017',
+              'period': 'M06',
+              'periodName': 'June',
+              'value': '244.955',
+              'footnotes': [{}]
+            },
+            {
+              'year': '2017',
+              'period': 'M05',
+              'periodName': 'May',
+              'value': '244.733',
+              'footnotes': [{}]
+            },
+            {
+              'year': '2017',
+              'period': 'M04',
+              'periodName': 'April',
+              'value': '244.524',
+              'footnotes': [{}]
+            },
+            {
+              'year': '2017',
+              'period': 'M03',
+              'periodName': 'March',
+              'value': '243.801',
+              'footnotes': [{}]
+            },
+            {
+              'year': '2017',
+              'period': 'M02',
+              'periodName': 'February',
+              'value': '243.603',
+              'footnotes': [{}]
+            },
+            {
+              'year': '2017',
+              'period': 'M01',
+              'periodName': 'January',
+              'value': '242.839',
+              'footnotes': [{}]
+            },
+            {
+              'year': '2016',
+              'period': 'M12',
+              'periodName': 'December',
+              'value': '241.432',
+              'footnotes': [{}]
+            },
+            {
+              'year': '2016',
+              'period': 'M11',
+              'periodName': 'November',
+              'value': '241.353',
+              'footnotes': [{}]
+            },
+            {
+              'year': '2016',
+              'period': 'M10',
+              'periodName': 'October',
+              'value': '241.729',
+              'footnotes': [{}]
+            },
+            {
+              'year': '2016',
+              'period': 'M09',
+              'periodName': 'September',
+              'value': '241.428',
+              'footnotes': [{}]
+            },
+            {
+              'year': '2016',
+              'period': 'M08',
+              'periodName': 'August',
+              'value': '240.849',
+              'footnotes': [{}]
+            },
+            {
+              'year': '2016',
+              'period': 'M07',
+              'periodName': 'July',
+              'value': '240.628',
+              'footnotes': [{}]
+            },
+            {
+              'year': '2016',
+              'period': 'M06',
+              'periodName': 'June',
+              'value': '241.018',
+              'footnotes': [{}]
+            },
+            {
+              'year': '2016',
+              'period': 'M05',
+              'periodName': 'May',
+              'value': '240.229',
+              'footnotes': [{}]
+            },
+            {
+              'year': '2016',
+              'period': 'M04',
+              'periodName': 'April',
+              'value': '239.261',
+              'footnotes': [{}]
+            },
+            {
+              'year': '2016',
+              'period': 'M03',
+              'periodName': 'March',
+              'value': '238.132',
+              'footnotes': [{}]
+            },
+            {
+              'year': '2016',
+              'period': 'M02',
+              'periodName': 'February',
+              'value': '237.111',
+              'footnotes': [{}]
+            },
+            {
+              'year': '2016',
+              'period': 'M01',
+              'periodName': 'January',
+              'value': '236.916',
+              'footnotes': [{}]
+            },
+            {
+              'year': '2015',
+              'period': 'M12',
+              'periodName': 'December',
+              'value': '236.525',
+              'footnotes': [{}]
+            },
+            {
+              'year': '2015',
+              'period': 'M11',
+              'periodName': 'November',
+              'value': '237.336',
+              'footnotes': [{}]
+            },
+            {
+              'year': '2015',
+              'period': 'M10',
+              'periodName': 'October',
+              'value': '237.838',
+              'footnotes': [{}]
+            },
+            {
+              'year': '2015',
+              'period': 'M09',
+              'periodName': 'September',
+              'value': '237.945',
+              'footnotes': [{}]
+            },
+            {
+              'year': '2015',
+              'period': 'M08',
+              'periodName': 'August',
+              'value': '238.316',
+              'footnotes': [{}]
+            },
+            {
+              'year': '2015',
+              'period': 'M07',
+              'periodName': 'July',
+              'value': '238.654',
+              'footnotes': [{}]
+            },
+            {
+              'year': '2015',
+              'period': 'M06',
+              'periodName': 'June',
+              'value': '238.638',
+              'footnotes': [{}]
+            },
+            {
+              'year': '2015',
+              'period': 'M05',
+              'periodName': 'May',
+              'value': '237.805',
+              'footnotes': [{}]
+            },
+            {
+              'year': '2015',
+              'period': 'M04',
+              'periodName': 'April',
+              'value': '236.599',
+              'footnotes': [{}]
+            },
+            {
+              'year': '2015',
+              'period': 'M03',
+              'periodName': 'March',
+              'value': '236.119',
+              'footnotes': [{}]
+            },
+            {
+              'year': '2015',
+              'period': 'M02',
+              'periodName': 'February',
+              'value': '234.722',
+              'footnotes': [{}]
+            },
+            {
+              'year': '2015',
+              'period': 'M01',
+              'periodName': 'January',
+              'value': '233.707',
+              'footnotes': [{}]
+            }]
+          }]
+        }
+      }
+
+      nock('https://api.bls.gov')
+        .post('/publicAPI/v2/timeseries/data/')
+        .reply(200, singleSeriesResponse)
+    })
+
     it('should return the right status', function () {
       cpiu.singleSeries().then(function (res) {
-        assert(res.status === 'REQUEST_SUCCEEDED')
+        console.log(res)
+        expect(res.status).to.equal('REQUEST_SUCCEEDED')
+      }).catch(function (err) {
+        console.log(err)
       })
     })
-    */
   })
 
   describe('singleSeriesWithOptions', function () {
-    /*
     it('should return the right status', function () {
       cpiu.singleSeriesWithOptions(1999, 2000).then(function (res) {
-        assert(res.status === 'REQUEST_SUCCEEDED')
+        expect(res.status).to.equal('REQUEST_SUCCEEDED')
       })
     })
-    */
   })
 })
